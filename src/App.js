96386b69408d84f449a20ecdeff3aaaeecbc67fa
Tom Nguyen <tomng@cs.stanford.edu>
2020-04-12 11:34:22 -0700

    Basic Auth0 support for user sign-in.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./styles.css";
+
 import FramedPhoto from "./FramedPhoto";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
 export default function App() {
+  const { loading } = useAuth0();
+
+  /* Need to avoid error on page load */
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <FramedPhoto />
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact>
+            <FramedPhoto />
+          </Route>
+          <Route path="/profile" component={Profile} />
+        </Switch>
+        <NavBar />
+      </Router>
     </div>
   );
 }
