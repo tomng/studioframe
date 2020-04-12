d7e82845d7adff352becbeaad07b48370a397d3d
Tom Nguyen <tomng@cs.stanford.edu>
2020-04-11 20:19:31 -0700

    Refactor to use React functions and states.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="parent ken-burns-image-wrap">
-        <img
-          className="framed"
-          src="https://images.unsplash.com/photo-1463936575829-25148e1db1b8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=thomas-verbruggen-5A06OWU6Wuc-unsplash.jpg&w=2400"
-        />
+      <FramedPhoto />
+    </div>
+  );
+}
+
+function FramedPhoto() {
+  const [getImageUrl, setImageUrl] = useState(
+    "https://images.unsplash.com/photo-1463936575829-25148e1db1b8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=thomas-verbruggen-5A06OWU6Wuc-unsplash.jpg&w=2400"
+  );
+
+  return (
+    <div className="framedPhoto">
+      <div className="photoContainer ken-burns-image-wrap">
+        <img alt="Framed Photograph" src={getImageUrl} />
       </div>
     </div>
   );
